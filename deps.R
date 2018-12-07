8d15d6d5fdb252309ce6b43adeab6bbc5fb747da
Toni Hermoso Pulido <toniher@cau.cat>
2018-12-07 11:45:57 +0100

    bioconductor

--- deps.R
+++ deps.R
@@ -1,6 +1,6 @@
 install.packages('gplots',dependencies=TRUE, repos='http://cran.rstudio.com/');
-install.packages('Heatplus',dependencies=TRUE, repos='http://cran.rstudio.com/');
-install.packages('HiTC',dependencies=TRUE, repos='http://cran.rstudio.com/');
 install.packages('MCMCpack',dependencies=TRUE, repos='http://cran.rstudio.com/');
 install.packages('RColorBrewer',dependencies=TRUE, repos='http://cran.rstudio.com/'); 
 
+install.packages("BiocManager",dependencies=TRUE, repos='http://cran.rstudio.com/');
+BiocManager::install(c("Heatplus", "HiTC"))
